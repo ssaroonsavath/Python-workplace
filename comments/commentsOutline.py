07c018df13e9e293fda8b2397fbfcc83d09546c0
ssaroonsavath <71401549+ssaroonsavath@users.noreply.github.com>
2020-10-14 22:58:54 -0500

    summary

---
+++ comments/commentsOutline.py
@@ -0,0 +1,17 @@
+'''
+This outline will help solidify concepts from the Comments lesson.
+Fill in this outline as the instructor goes through the lesson.
+'''
+
+#1) Make a single line comment.
+
+
+
+#2) Make a multi-line comment.
+
+
+
+#3) Make an int variable. Then, make a single line comment on the 
+#same line, after the variable.
+
+
---
+++ comments/practiceComments.py
@@ -0,0 +1,22 @@
+'''
+Created on Oct 14, 2020
+
+@author: ITAUser
+'''
+
+#dahfjkhjdshflhwunekdklajkfadjfl
+
+'''dkjalkfjdlk;jalf
+dsgsdaf
+sgag
+ahdahkla
+ahkeajgdfa
+famgkemak
+sfdafd
+agakgd
+
+#This isn't working, can't debug, what do you think?
+
+var = 4 #single line comment 
+print(var)'''
+
