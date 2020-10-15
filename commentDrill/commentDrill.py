f37576177f40c25d2bdaecd8f568447863edef6e
ssaroonsavath <71401549+ssaroonsavath@users.noreply.github.com>
2020-10-14 23:25:55 -0500

    summary

---
+++ commentDrill/commentDrill.py
@@ -0,0 +1,23 @@
+
+#This is a single line coment
+
+
+''' 
+this 
+is a 
+mult-line Comment
+hi itaa 
+how is you going??
+
+'''
+
+var = 3 #I'm so tired
+
+#daklgjdl;akmgeklnalkndlj hi
+
+'''I promise i'm
+not a kboo
+but stan BTS 
+and stream Dynatime 
+hehehe'''
+
