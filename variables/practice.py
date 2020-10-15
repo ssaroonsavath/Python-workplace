5c7251bbc9c32a0181cfdb71ed41b77db8f3d5f8
ssaroonsavath <71401549+ssaroonsavath@users.noreply.github.com>
2020-10-14 22:14:40 -0500

    variables

---
+++ variables/practice.py
@@ -0,0 +1,110 @@
+'''
+
+[name] = [data]
+
+
+'''
+
+#Integer 
+
+a = 5 
+a= 4
+
+#Float
+
+a = 3.45
+a = 2.1 
+
+#String 
+
+a = "string"
+name = "savannah"
+
+#Boolean 
+
+a =True 
+a = False 
+
+#List 
+
+a = [3, 4, 5, 6]
+
+a = ["stringOne", "stringTwo"]
+
+a = [True, False, True]
+
+a = [1.1, 2.2, 3.3]
+
+
+
+varA = 1
+vara= 2
+
+print(varA)
+
+print(a)
+
+
+
+var = 4 
+var = "string"
+
+varTwo = var 
+print(varTwo)
+
+b = [a, varA, var]
+
+list = [ "a", "b", "c"]
+
+print(list)
+print(list[0])
+
+listVar = list[0]
+print(listVar)
+
+string = "savannah"
+
+print(string[0])
+
+print(5)
+print("string")
+print(True)
+print(list)
+print(string)
+
+'''
+Syntax 
+
+Logical 
+
+'''
+
+var = 4 
+name = 4
+
+name = "micheal"
+
+varThree = 0 
+
+var = varThree
+
+micheal = "name"
+
+print(micheal)
+
+titles = "micheal"
+
+print(titles)
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ variables/variablesOutline.py
@@ -0,0 +1,48 @@
+'''
+This outline will help solidify concepts from the Variables lesson.
+Fill in this outline as the instructor goes through the lesson.
+'''
+
+#1)
+#Make an example variable of each of the types of variables (int, float, 
+#string, boolean and list):
+
+
+
+#2)
+#Make two variables one named 'a' and one named 'A' both set to  different data:
+#(Remember that capitalization matters, so these can be two different variables)
+
+
+
+#3)
+#Make a variable, and then go down a couple lines and change the data in that
+#same variable:
+
+
+#4)
+#Make a variable and set it to a variable you have already created:
+
+
+
+#5)
+#Make a list with three items. Make a variable that is set to the second
+#item in that list:
+
+
+
+#6)
+#Make a String set to your name. Make a variable that is set to the first item
+#in that list:
+
+
+
+#7)
+#Print out ONE of any of the variables you have created so far:
+
+
+
+#8)
+#Make a variable of any type, with one of the common SYNTAX errors
+
+
