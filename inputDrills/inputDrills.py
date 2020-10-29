fa358f0bc73aac58de6b33f893ff1ac9c001eb30
ssaroonsavath <71401549+ssaroonsavath@users.noreply.github.com>
2020-10-29 14:55:24 -0500

    summary

---
+++ inputDrills/inputDrills.py
@@ -0,0 +1,24 @@
+'''
+Created on Oct 29, 2020
+
+@author: ITAUser
+'''
+
+userResponse = input("What is your favorite animal?")
+favAnimal = input("wolf")
+
+
+userResponse = input("What is your favorite number?")
+favNum = input("18")
+
+
+
+userResponse = input("What is your school's name?")
+school = input("East High School")
+
+
+userResponse = input("What is your instructors name?")
+instructor = input("Micheal and Chokyi")
+
+userResponse = input("What is 2 +2?")
+ans = input("4")
