37130857b9bd4ca72991e0e84ad255ef3e0ff925
ssaroonsavath <71401549+ssaroonsavath@users.noreply.github.com>
2020-10-29 15:51:50 -0500

    summary

---
+++ survey/survey.py
@@ -0,0 +1,47 @@
+'''
+Make a program that can take survey information
+
+@author: Savannah Aroonsavath
+'''
+
+#Use input() to ask the user "What is your name?" and store that in
+#a variable called name. Then print the variable.
+
+userResponse = input("What is your name?")
+name ="Savannah Aroonsavath"
+print(name)
+
+
+#Use input() to ask the user "What is your favorite color?" and store that 
+#in a variable called color. Then print the variable.
+
+Response = input("What is your favorite color?")
+color = "Greyish blue"
+print(color)
+
+
+#Use input() to ask the user "What city did you grow up in?" and store 
+#that in a variable called city. Then print the variable.
+
+userResponse = input("What city did you grow up in?")
+city = "Madison"
+print(city)
+
+
+#Use input() to ask the user "What is your best friends name?" and store 
+#that in a variable called friend. Then print the variable.
+
+userResponse = input("What is your best friend's name?")
+friend = "Destiny, Seesia and Jae"
+print(friend)
+
+
+#Use input() to ask the user "What color hair do you have?" and store that 
+#in a variable called hair. Then print the variable.
+
+userResponse = input("What color hair do you have?")
+hair = "Dark brown"
+print(hair)
+
+
+
