7de7d8d42271f4903290466120d1d0b09a010217
ssaroonsavath <71401549+ssaroonsavath@users.noreply.github.com>
2020-11-19 14:09:25 -0600

    summary

---
+++ comparisonOperators/comparisonOutline.py
@@ -0,0 +1,38 @@
+'''
+This outline will help solidify concepts from the Comparison Operators lesson.
+Fill in this outline as the instructor goes through the lesson.
+'''
+
+#1) Make two string variables. Compare them using the == operator and store
+#that comparison in a new variable. Then print the variable.
+
+
+
+#2) Make two int variables. Compare them using the == operator and store
+#that comparison in a new variable. Then print the variable.
+
+
+
+#3) Make two int variables. Compare them using the > operator and store
+#that comparison in a new variable. Then print the variable.
+
+
+
+#4) Make two int variables. Compare them using the < operator and store
+#that comparison in a new variable. Then print the variable.
+
+
+
+#5) Make two int variables. Compare them using the >= operator and store
+#that comparison in a new variable. Then print the variable.
+
+
+
+#6) Make two int variables. Compare them using the <= operator and store
+#that comparison in a new variable. Then print the variable.
+
+
+
+#7) Make a comparison with any operator, with one of the common SYNTAX errors.
+
+
---
+++ comparisonOperators/practice.py
@@ -0,0 +1,56 @@
+'''
+==
+<
+>
+<=
+>=
+'''
+
+4 == 4
+#True
+
+x = 4 
+
+x == 4 
+#True
+
+2 == 4
+#False
+
+2 < 3
+#True 
+
+3 < 2
+#False 
+
+4 > 3 
+#True 
+
+3 > 4 
+#False
+
+3 <= 4 
+#True 
+
+4 <= 4
+#True
+
+4 >= 3 
+#True 
+
+4 >= 4
+#True
+
+3 >= 4
+#False
+
+v = (4 == 5)
+print(v)
+
+3 >= 4
+
+x = "micheal" == "tenzin"
+print(x)
+
+x = "micheal" == "micheal"
+print(x)
